329ccf20be921d9647d2aea924d9dc5336c36532
Kirill Sagorin <st128530@student.spbu.ru>
2025-10-07 02:52:34 +0300

    missed that i is in two separred cycles

--- gtest.cpp
+++ gtest.cpp
@@ -67,9 +67,9 @@ TEST(FundSolutionTest, NoFreeVariables)
 
 TEST(AllFunctions, RandomInput)
 {
-    for(int i = 1; i <= 15; i+=5)
+    for(int h = 1; h <= 15; h+=5)
     {
-        int a = 10 * i, b = 12 * i;
+        int a = 10 * h, b = 12 * h;
         createFile("test_getMatrix.csv", a, b);
         std::vector<std::vector<double>> m = getMatrix("test_getMatrix.csv");
         std::vector<std::vector<double>> s = fundSolution(gaussMethod(m));
@@ -86,19 +86,6 @@ TEST(AllFunctions, RandomInput)
                 EXPECT_NEAR(sum, 0, 2e-10);
             }
         }
-        /*for(int j = 0; j < m[0].size(); j++)
-        {
-        std::cout<<"test "<<j<<std::endl;
-            for(int k = 0; k < s.size(); k++)
-            {
-                double a = 0;
-                for(int l = 0; l < s[0].size(); l++)
-                {
-                    a += m[k][l] * s[k][l];
-                }
-                EXPECT_NEAR(a, 0, 2e-10);
-            }
-        }*/
     }
 }
 
